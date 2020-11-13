4a9bad167c47e99a18d003b7f0683a56fd30266a
Emma Blott <blott23@gmail.com>
2020-11-13 19:35:38 +0000

    Create map which renders all pokemon in list

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function PokemonList( {pokemon}) {
   return (
     <div>
-      
+      {pokemon.map(p => (
+        <div key={p}>{p}</div>
+      ))}
     </div>
   )
 }
