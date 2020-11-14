27b219ca4e74f14ee48f1e845cd81d6b487a129a
Emma Blott <blott23@gmail.com>
2020-11-14 15:48:22 +0000

    Incorrectly use axios.get to return a promise containing the JSON & setPokemon to a map of this

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import axios from 'axios'
 
 function App() {
 
-  const [pokemon, setPokemon] = useState()
+  const [pokemon, setPokemon] = useState([])
+
+  axios.get("https://pokeapi.co/api/v2/pokemon").then(res =>{
+    setPokemon(res.data.results.map(p => p.name))
+  })
 
   return (
     <PokemonList pokemon={pokemon} />
