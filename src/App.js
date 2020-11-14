e60a3920501ab745717849dc427359194a330fc4
Emma Blott <blott23@gmail.com>
2020-11-14 16:00:05 +0000

    Create and fill next and prev PageUrl components
    We create new state variables which are `nextPageUrl` and `prevPageUrl`.
    The inital state of these is set to nothing, we then populate them using
    values from the API we call in useEffect.
    
    The json response contains variables, next & previous, which contain the
    url of the next 20 or previous 20 paginated results.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ function App() {
 
   const [pokemon, setPokemon] = useState([])
   const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
 
   useEffect(()=> {
     axios.get(currentPageUrl).then(res =>{
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p => p.name))
     })
   },[currentPageUrl])
